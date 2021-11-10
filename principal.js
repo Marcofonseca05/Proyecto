a8d323714934586b0c9ec8bd397c94b8804d894a
estefi21 <90219265+estefi21@users.noreply.github.com>
2021-11-10 15:15:26 -0300

    nashe

---
+++ principal.js
@@ -0,0 +1,19 @@
+function traerPeliculas() {
+    fetch("peliculas.json")
+        .then(respuesta => respuesta.json())
+        .then(datosEst => {
+            escribe=``
+            console.log("dataEst", datosEst);
+            for(peli of datosEst.pelis){
+                document.getElementById("acaPostre").innerHTML = escribe+= 
+                `<div class='col-2'>
+                    <img src='${peli.imagen}'  style='width:200px; height: 200px'>
+                    <h3>${peli.nombre}</h3> 
+                </div>`
+            } 
+            //mostrarPeliculas(datosEst);
+        }).catch(error => {
+            console.log("Error", error.message);
+        });
+};
+traerPeliculas();
